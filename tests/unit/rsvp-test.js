caad24404a0bc36dcde3d3331e543c89f9a70263
Jonathan Jackson <jonathan.jackson1@me.com>
2014-07-12 13:53:14 -0400

    Setup RSVP project config

---
+++ tests/unit/rsvp-test.js
@@ -0,0 +1,23 @@
+var projectFileMap = require("../../project_configs/rsvp-proj");
+var assert = require("assert");
+
+describe('rsvp.projectFileMap', function(){
+  it('returns appropriate locations for master', function(){
+    expectedLocations = [
+      'rsvp-latest.js',
+      'rsvp-foo-commit.js',
+      'rsvp-latest.amd.js',
+      'rsvp-foo-commit.amd.js'
+    ];
+
+    var files = projectFileMap('foo-commit', 'foo-tag');
+    var uploadFileLocations = [];
+
+    for (var file in files) {
+      files[file].destinations.wildcard.forEach(function(destination){
+        uploadFileLocations.push(destination);
+      });
+    }
+    assert.deepEqual(expectedLocations, uploadFileLocations);
+  });
+});
